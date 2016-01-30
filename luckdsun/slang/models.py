2add6ce80f1795b71655b619bcf44be1fa929ae5
Moon Gwanghun <mkh9209@naver.com>
2016-01-30 16:01:17 +0900

    회원, 방 models.py 추가

--- luckdsun/slang/models.py
+++ luckdsun/slang/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    m_id = models.CharFiled.unique(max_length=10)
+    m_email = models.EmailFiled.unique
+    m_pass = models.CharFiled
+    
+    def __str__(self):
+        return self.m_id
+    
+class Team(models.Model):
+    t_name = models.CharFiled.unique(max_length=20)
+    
+    def __str__(self):
+        return self.t_name
