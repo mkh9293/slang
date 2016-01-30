08302f02125b3e8f2f4d6697e4e7eef7ab1778ee
Moon Gwanghun <mkh9209@naver.com>
2016-01-30 16:22:25 +0900

    models 다대다로 수정

--- luckdsun/slang/models.py
+++ luckdsun/slang/models.py
@@ -4,15 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Member(models.Model):
-    m_id = models.CharFiled.unique(max_length=10)
-    m_email = models.EmailFiled.unique
-    m_pass = models.CharFiled
+    m_id = models.CharField(max_length=10,unique=True)
+    m_email = models.EmailField.unique
+    m_pass = models.CharField
     
     def __str__(self):
         return self.m_id
     
 class Team(models.Model):
-    t_name = models.CharFiled.unique(max_length=20)
+    t_name = models.CharField(max_length=20,unique=True)
+    m_idx = models.ManyToManyField(Member)
     
     def __str__(self):
         return self.t_name
