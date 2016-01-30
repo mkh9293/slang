8daaa1d0183234a0ec4e6b07a24b2225a6c1f8ed
dudrnxps <dudrnxps1@nate.com>
2016-01-30 18:23:25 +0900

    Revert "최종 회원, 방 models.py 완성"
    This reverts commit a52aa22a16dbb75a97c49c2c86d2c5ecb3ac5ad9.

--- luckdsun/slang/admin.py
+++ luckdsun/slang/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-
+<<<<<<< HEAD
 from slang.models import Member,Team
 # Register your models here.
 
 admin.site.register(Member)
 admin.site.register(Team)
+=======
+from slang.models import Member, Team
 
+admin.site.register(Member)
+admin.site.register(Team)
+>>>>>>> 85f3526f7f9ed2b98ec2a522110de12489ec190e
