699727d312c3781d2cad028543affdf918c43939
dudrnxps <dudrnxps1@nate.com>
2016-01-30 18:17:34 +0900

    예전것

--- luckdsun/slang/models.py
+++ luckdsun/slang/models.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Member(models.Model):
-    m_id = models.CharField(max_length=10,unique=True)
-    m_email = models.EmailField.unique
-    m_pass = models.CharField
+    m_id = models.CharField(max_length=10, unique=True)
+    m_mail = models.EmailField
+    m_pass = models.CharField(max_length=10)
     
     def __str__(self):
         return self.m_id
